eace6fa5063868aead19caea0937913dc47620e7
Yurei TZK <me@yureitzk.com>
2025-08-27 00:02:27 +0300

    chore: fix aliases for svelte

--- svelte.config.js
+++ svelte.config.js
@@ -3,5 +3,14 @@ import sveltePreprocess from 'svelte-preprocess';
 export default {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: sveltePreprocess(),
+	preprocess: [sveltePreprocess({ typescript: true })],
+	extensions: ['.svelte'],
+	compilerOptions: {},
+	onwarn: (warning, handler) => handler(warning),
+	// plugin options
+	vitePlugin: {
+		exclude: [],
+		// experimental options
+		experimental: {},
+	},
 };
